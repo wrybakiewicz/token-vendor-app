17d96a287fe0b7f0505a1fdf2a9734f90269184e
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-02-28 15:46:47 +0100

    contract: add withdraw function

--- contracts/Vendor.sol
+++ contracts/Vendor.sol
@@ -2,13 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "./BugCoin.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Vendor {
+contract Vendor is Ownable {
 
     BugCoin public bugCoin;
     uint public immutable bugCoinPerEth = 10;
 
     event TokenBought(address buyer, uint amountOfEth, uint amountOfBugCoin);
+    event EthWithdrawn(address owner, uint amountOfEth);
 
     constructor(address _bugCoin) {
         bugCoin = BugCoin(_bugCoin);
@@ -18,5 +20,11 @@ contract Vendor {
         uint tokens = msg.value * bugCoinPerEth;
         bugCoin.transfer(msg.sender, tokens);
         emit TokenBought(msg.sender, msg.value, tokens);
+    }
+
+    function withdraw() external onlyOwner {
+        uint balance = address(this).balance;
+        payable(msg.sender).transfer(balance);
+        emit EthWithdrawn(msg.sender, balance);
     }
 }
--- test/vendor.test.js
+++ test/vendor.test.js
@@ -1,20 +1,23 @@
 const {expect} = require("chai");
-const {ethers} = require("hardhat");
+const {ethers, waffle} = require("hardhat");
 const {BigNumber} = require("ethers");
 
 const bugCoinSupply = BigNumber.from(10).pow(18).mul(1000);
 
 describe("Vendor", function () {
     it("should deploy vendor with 0 balance", async function () {
+        const [owner, address1] = await ethers.getSigners();
         const bugCoin = await deployBugCoin();
         const Vendor = await ethers.getContractFactory("Vendor");
-        const vendor = await Vendor.deploy(bugCoin.address);
+        const vendor = await Vendor.connect(address1).deploy(bugCoin.address);
         await vendor.deployed();
 
         const receivedBugCoin = await vendor.bugCoin();
         expect(receivedBugCoin).to.equal(bugCoin.address);
         const vendorBalance = await bugCoin.balanceOf(vendor.address);
         expect(vendorBalance).to.equal(0);
+        const vendorOwner = await vendor.owner();
+        expect(vendorOwner).to.equal(address1.address);
     });
 
     it("should buy from vendor", async function () {
@@ -24,14 +27,24 @@ describe("Vendor", function () {
         const vendor = await Vendor.deploy(bugCoin.address);
         await vendor.deployed();
         const vendorBalance = ethers.utils.parseEther("10.5");
-        await bugCoin.transfer(vendor.address, vendorBalance)
+        await bugCoin.transfer(vendor.address, vendorBalance);
+        const ownerEthBalanceInitial = await owner.getBalance();
+        const address1EthBalanceInitial = await address1.getBalance();
+        const buyTxValue = ethers.utils.parseEther("1");
 
-        const buyTx = await vendor.connect(address1).buy({value: ethers.utils.parseEther("1")});
+        const buyTx = await vendor.connect(address1).buy({value: buyTxValue});
 
         const newVendorBalance = await bugCoin.balanceOf(vendor.address);
         expect(newVendorBalance).to.equal(ethers.utils.parseEther("0.5"));
-        const address1Balance = await bugCoin.balanceOf(address1.address);
-        expect(address1Balance).to.equal(ethers.utils.parseEther("10"));
+        const address1BalanceBgc = await bugCoin.balanceOf(address1.address);
+        expect(address1BalanceBgc).to.equal(ethers.utils.parseEther("10"));
+        const vendorEthBalance = await waffle.provider.getBalance(vendor.address);
+        expect(vendorEthBalance).to.equal(buyTxValue);
+        const ownerBalance = await owner.getBalance();
+        expect(ownerBalance).to.equal(ownerEthBalanceInitial);
+        const address1Balance = await address1.getBalance();
+        const buyGasFee = await getGasFee(buyTx);
+        expect(address1Balance).to.equal(address1EthBalanceInitial.sub(buyTxValue).sub(buyGasFee));
         expect(buyTx).to.emit(vendor, "TokenBought")
             .withArgs(address1.address, ethers.utils.parseEther("1"), ethers.utils.parseEther("10"));
     });
@@ -44,13 +57,23 @@ describe("Vendor", function () {
         await vendor.deployed();
         const vendorBalance = ethers.utils.parseEther("10.5");
         await bugCoin.transfer(vendor.address, vendorBalance)
+        const buyTxValue = vendorBalance.div(10);
+        const ownerEthBalanceInitial = await owner.getBalance();
+        const address1EthBalanceInitial = await address1.getBalance();
 
-        const buyTx = await vendor.connect(address1).buy({value: vendorBalance.div(10)});
+        const buyTx = await vendor.connect(address1).buy({value: buyTxValue});
 
         const newVendorBalance = await bugCoin.balanceOf(vendor.address);
         expect(newVendorBalance).to.equal(0);
-        const address1Balance = await bugCoin.balanceOf(address1.address);
-        expect(address1Balance).to.equal(ethers.utils.parseEther("10.5"));
+        const address1BgcBalance = await bugCoin.balanceOf(address1.address);
+        expect(address1BgcBalance).to.equal(ethers.utils.parseEther("10.5"));
+        const vendorEthBalance = await waffle.provider.getBalance(vendor.address);
+        expect(vendorEthBalance).to.equal(buyTxValue);
+        const ownerBalance = await owner.getBalance();
+        expect(ownerBalance).to.equal(ownerEthBalanceInitial);
+        const address1Balance = await address1.getBalance();
+        const buyGasFee = await getGasFee(buyTx);
+        expect(address1Balance).to.equal(address1EthBalanceInitial.sub(buyTxValue).sub(buyGasFee));
         expect(buyTx).to.emit(vendor, "TokenBought")
             .withArgs(address1.address, ethers.utils.parseEther("1.05"), ethers.utils.parseEther("10.5"));
     });
@@ -70,10 +93,58 @@ describe("Vendor", function () {
         await expect(buyTx).to.be.revertedWith("ERC20: transfer amount exceeds balance");
     });
 
+    it("should buy from vendor & withdraw eth", async function () {
+        const [owner, address1] = await ethers.getSigners();
+        const bugCoin = await deployBugCoin();
+        const Vendor = await ethers.getContractFactory("Vendor");
+        const vendor = await Vendor.deploy(bugCoin.address);
+        await vendor.deployed();
+        const vendorBgcBalance = ethers.utils.parseEther("10.5");
+        await bugCoin.transfer(vendor.address, vendorBgcBalance);
+        const address1EthBalanceInitial = await address1.getBalance();
+        const vendorEthBalanceInitial = await owner.getBalance();
+        const buyTxValue = ethers.utils.parseEther("1");
+
+        const buyTx = await vendor.connect(address1).buy({value: buyTxValue});
+        const withdrawTx = await vendor.connect(owner).withdraw();
+
+        const vendorEthBalance = await waffle.provider.getBalance(vendor.address);
+        expect(vendorEthBalance).to.equal(0);
+        const ownerEthBalance = await owner.getBalance();
+        const withdrawGasFee = await getGasFee(withdrawTx);
+        expect(ownerEthBalance).to.equal(vendorEthBalanceInitial.sub(withdrawGasFee).add(buyTxValue));
+        const address1Balance = await address1.getBalance();
+        const buyGasFee = await getGasFee(buyTx);
+        expect(address1Balance).to.equal(address1EthBalanceInitial.sub(buyTxValue).sub(buyGasFee));
+        expect(withdrawTx).to.emit(vendor, "EthWithdrawn")
+            .withArgs(owner.address, ethers.utils.parseEther("1"));
+    });
+
+    it("should buy from vendor & fail to withdraw eth not by owner", async function () {
+        const [owner, address1] = await ethers.getSigners();
+        const bugCoin = await deployBugCoin();
+        const Vendor = await ethers.getContractFactory("Vendor");
+        const vendor = await Vendor.deploy(bugCoin.address);
+        await vendor.deployed();
+        const vendorBgcBalance = ethers.utils.parseEther("10.5");
+        await bugCoin.transfer(vendor.address, vendorBgcBalance);
+        const buyTxValue = ethers.utils.parseEther("1");
+
+        await vendor.connect(address1).buy({value: buyTxValue});
+        const withdrawTx = vendor.connect(address1).withdraw();
+
+        await expect(withdrawTx).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
 })
 
 function deployBugCoin() {
     return ethers.getContractFactory("BugCoin")
         .then(bugCoin => bugCoin.deploy(bugCoinSupply))
         .then(bugCoinDeploy => bugCoinDeploy.deployed())
+}
+
+function getGasFee(tx) {
+    return tx.wait()
+        .then(txResult => txResult.effectiveGasPrice.mul(txResult.cumulativeGasUsed));
 }
